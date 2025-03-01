b31e973823f869b5680457c85b0ea3e261280504
Tuan Quang <bibuiban123@gmail.com>
2025-03-01 15:03:56 -0500

    custom range for color red

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import numpy as np
 
 image = cv.imread('./train/1000_F_409785284_0SeCTRiQ0M5ASa4TlpDdrbsMIJSAvC0l_jpg.rf.20f26de85cca598302a0589e6d4071f2.jpg')
 cv.imwrite('./test.jpg', image)
-hsv_image = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+
+hsv_image = cv.cvtColor(image, cv.COLOR_RGB2HSV)
 
 red = np.uint8([[[0,0,240 ]]])
 hsv_red = cv.cvtColor(red,cv.COLOR_BGR2HSV)
 
+delta = 5
+common = 125
+
 # define range of red color in HSV
-lower_red = np.array([0, 100, 100])
-upper_red = np.array([10, 255, 255])
+lower_red = np.array([common- delta, 0, 0])
+upper_red = np.array([common + delta, 255, 255])
 
 # Threshold the HSV image to get only blue colors
 mask = cv.inRange(hsv_image, lower_red, upper_red)
